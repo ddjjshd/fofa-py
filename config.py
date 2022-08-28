ae443fcb9c5bc03804ceab7aa04b0de0e042b62d
ddjjshd <112316535+ddjjshd@users.noreply.github.com>
2022-08-28 23:06:58 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+
+
+
+cookie=""
+
+headers = {
+        "Connection": "keep-alive",
+        "cookie": cookie.encode("utf-8").decode("latin1")
+    }
+
+SearchKEY=""
+StopPage=""
+StartPage=""
+TimeSleep=5
